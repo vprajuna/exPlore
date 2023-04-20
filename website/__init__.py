57f1de9c3a3247165eeb190f4976742f3223262d
peteyyy <lexsp25@gmail.com>
2023-04-19 22:59:33 -0400

    Fixed asset paths

--- website/__init__.py
+++ website/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
-    app = Flask(__name__) 
+    app = Flask(__name__, static_folder="static") 
     app.config['SECRET_KEY'] = 'random key string'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
